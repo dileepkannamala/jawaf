e72ad4e9bd6ea6eb79e775ee73458f5e1322c2a4
Daniel Pozmanter <danpozmanter+github@gmail.com>
2018-12-20 00:04:44 -0500

    debugging

--- tests/utils/test_utils_timezone.py
+++ tests/utils/test_utils_timezone.py
@@ -17,6 +17,12 @@ def test_get_local():
     """Test converting from utc to local."""
     target = datetime.datetime(2017, 4, 11, 8, 0, tzinfo=pytz.utc)
     local_target = timezone.get_local(target)
+    print('-------------')
+    print('local')
+    print(time.localtime().tm_isdst > 0)
+    print(UTC_OFFSET)
+    print(target)
+    print(local_target)
     assert local_target.hour == int(8 + UTC_OFFSET)
 
 
@@ -24,5 +30,11 @@ def test_get_utc():
     """Test converting a naive datetime -> utc datetime with timezone info."""
     target = datetime.datetime(2017, 4, 11, 0, 0)
     utc_target = timezone.get_utc(target)
+    print('-------------')
+    print('utc')
+    print(time.localtime().tm_isdst > 0)
+    print(UTC_OFFSET)
+    print(target)
+    print(utc_target)
     assert utc_target.tzinfo == pytz.utc
     assert utc_target.hour == int(0 - UTC_OFFSET)
